041c88202bdbb653f42e79712821a82b00ed2f3e
TingBoy <tingboy@gmail.com>
2016-11-29 12:57:45 -0800

    test

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send("Hellow World from server")
+	res.send("Hello World from server")
 });
 
 app.listen(8080);
-console.log("Server running on port 3000");
+console.log("Server running on port 8080");
