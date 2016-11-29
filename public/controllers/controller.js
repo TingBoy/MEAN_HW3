81c5f632e8c57b44d9dad1b1e79e26438dfabbe3
TingBoy <tingboy@gmail.com>
2016-11-29 14:38:32 -0800

    test

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,8 +5,8 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 	var refresh = function() {
 		$http.get('/resume/').success(function (response) {
 			console.log("I got the data I requested");
-			$scope.resume = response;
-			$scope.listing = "";
+			$scope.schools = response;
+			$scope.school = "";
 		});
 	};
 	
